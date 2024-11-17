23abadcc5c7599234ec161b8e296ab358b7d59c6
sergiogzz <sergio.gzzbrz@gmail.com>
2024-11-17 15:34:11 -0600

    Fix Cmake and add unit testing

---
+++ app/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <algorithm>
+
+#include "parte1_1.h"
+#include "parte1_2.h"
+#include "parte2.h"
+#include "parte3.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Parte 1:" << endl;
+
+    part1_1("transmission1.txt", "mcode1.txt");
+    part1_1("transmission1.txt", "mcode2.txt");
+    part1_1("transmission1.txt", "mcode3.txt");
+    part1_2("transmission2.txt", "mcode1.txt");
+    part1_2("transmission2.txt", "mcode2.txt");
+    part1_2("transmission2.txt", "mcode3.txt");
+
+    cout << endl;
+    cout << "Parte 2:" << endl;
+    manacher("transmission1.txt");
+    manacher("transmission2.txt");
+
+    cout << "Parte 3:" << endl;
+    part3("transmission1.txt", "transmission2.txt");
+}
---
+++ tests/test_part1.cc
@@ -0,0 +1,12 @@
+#include <catch2/catch_test_macros.hpp>
+#include <vector>
+#include <string>
+#include "parte1_1.h"
+
+TEST_CASE("transmission1 with mcode 1")
+{   // Apply Z algorithm to find all occurrences of mcode in transmission
+    vector<int> ans = ZAlg1("ab", "hasta la ab abab");
+    vector<int> realAns = {0,0,0,0,1,0,0,1,0,0,1,0,2,0,0,2,0,2,0};
+
+    REQUIRE(ans == realAns);
+}
